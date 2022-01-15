9ff842997a979de3543c01e385694ef0cd9b4f66
Axel <axesode+github@gmail.com>
2022-01-15 18:01:48 +0100

    fixed shiftmask

--- config.def.h
+++ config.def.h
@@ -104,7 +104,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                 			XK_y,  	   togglescratch,  {.ui = 0 } },
-	{ MODKEY|Shiftmask,        			XK_Return, togglescratch,  {.ui = 1 } },
+	{ MODKEY|ShiftMask,        			XK_Return, togglescratch,  {.ui = 1 } },
 	{ MODKEY,                 			XK_x,	     togglescratch,  {.ui = 2 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
